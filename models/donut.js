b203fc42cdbdae48c84201f144c74e8f4c66cae0
JurRodi <rodijurrien@gmail.com>
2022-11-27 09:38:11 +0100

    Feat: add model for donut with mongoose schema

---
+++ models/donut.js
@@ -0,0 +1,15 @@
+const Mongooose = require('mongoose');
+const Schema = Mongooose.Schema;
+
+const DonutSchema = new Schema({
+    id: {type: Number, required: true},
+    color: {type: String, required: true},
+    topping: {type: String, required: true},
+    logo: {type: String, required: true},
+    email: {type: String, required: true},
+    company: {type: String, required: true},
+});
+
+const Donut = Mongooose.model('Donut', DonutSchema);
+
+module.exports = Donut;
