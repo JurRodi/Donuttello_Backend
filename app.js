5726a74118728f55a9742b3a198aa35c19b48f6e
JurRodi <rodijurrien@gmail.com>
2022-11-27 09:22:32 +0100

    Feat: add donuts route & router and db connection

--- app.js
+++ app.js
@@ -4,8 +4,12 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/donuttello', {useNewUrlParser: true});
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var donutsRouter = require('./routes/donuts');
 
 var app = express();
 
@@ -21,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/API/v1/donuts', donutsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
