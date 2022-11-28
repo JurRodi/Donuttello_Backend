d5092264395e7f850eb1de9112f38f8ac6c8a602
JurRodi <rodijurrien@gmail.com>
2022-11-28 10:34:04 +0100

    Feat: add delete donut route

--- routes/donuts.js
+++ routes/donuts.js
@@ -4,5 +4,6 @@ const donutsCtrl = require('../controllers/donutsCtrl');
 
 router.get('/', donutsCtrl.getAllDonuts);
 router.post('/', donutsCtrl.createDonut);
+router.delete('/:id', donutsCtrl.deleteDonut);
 
 module.exports = router;
