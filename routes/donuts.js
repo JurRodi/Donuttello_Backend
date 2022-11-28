48b37fb1eb075437d634fdca08b3123bf2337b3c
JurRodi <rodijurrien@gmail.com>
2022-11-28 11:08:18 +0100

    Feat: add get one donut route

--- routes/donuts.js
+++ routes/donuts.js
@@ -6,5 +6,6 @@ router.get('/', donutsCtrl.getAllDonuts);
 router.post('/', donutsCtrl.createDonut);
 router.delete('/:id', donutsCtrl.deleteDonut);
 router.put('/:id', donutsCtrl.updateDonut);
+router.get('/:id', donutsCtrl.getDonutById);
 
 module.exports = router;
