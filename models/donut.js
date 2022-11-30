0552089377bd8f21197c13fd31289ac86d9ca8b4
JurRodi <rodijurrien@gmail.com>
2022-11-30 14:13:38 +0100

    Fix: adjust keys

--- models/donut.js
+++ models/donut.js
@@ -5,12 +5,12 @@ const DonutSchema = new Schema({
     name: {type: String, required: true},
     glace: {type: String, required: true},
     topping: {type: String, required: true},
-    logo: {type: String, required: true},
+    logo: {type: String, default: "none"},
     shape: {type: String, required: true},
     amount: {type: Number, required: true},
     email: {type: String, required: true},
     company: {type: String, required: true},
-    extra: {type: String, required: true},
+    extra: {type: String, default: "No extra information"},
     status: {type: String, default: "Aangevraagd"},
 });
 
