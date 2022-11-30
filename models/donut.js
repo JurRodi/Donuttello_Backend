4ca9238320c1a8e81a19cb3d178e624410b43970
JurRodi <rodijurrien@gmail.com>
2022-11-30 12:27:28 +0100

    Feat: update donut keys

--- models/donut.js
+++ models/donut.js
@@ -2,11 +2,16 @@ const Mongooose = require('mongoose');
 const Schema = Mongooose.Schema;
 
 const DonutSchema = new Schema({
-    color: {type: String, required: true},
+    name: {type: String, required: true},
+    glace: {type: String, required: true},
     topping: {type: String, required: true},
     logo: {type: String, required: true},
+    shape: {type: String, required: true},
+    amount: {type: Number, required: true},
     email: {type: String, required: true},
     company: {type: String, required: true},
+    extra: {type: String, required: true},
+    status: {type: String, default: "Aangevraagd"},
 });
 
 const Donut = Mongooose.model('Donut', DonutSchema);
