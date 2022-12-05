8f5faa39e21ff47a1de9c39f91bf8ac2c9c7ccf4
JurRodi <rodijurrien@gmail.com>
2022-12-05 21:08:25 +0100

    Feat: add login & changepassword routes & userctrl

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
+const usersCtrl = require('../controllers/usersCtrl');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.post('/login', usersCtrl.login);
+router.post('/changePassword', usersCtrl.changePassword);
 
 module.exports = router;
