41dad8402cdd19f5f1c22d50775babcb28806522
JurRodi <rodijurrien@gmail.com>
2022-12-09 12:46:05 +0100

    Feat: add authToken middleware to routes

--- routes/donuts.js
+++ routes/donuts.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var router = express.Router();
 const donutsCtrl = require('../controllers/donutsCtrl');
+const authenticateToken = require('../middleware/authenticateToken');
 
-router.get('/', donutsCtrl.getAllDonuts);
+router.get('/', authenticateToken, donutsCtrl.getAllDonuts);
 router.post('/', donutsCtrl.createDonut);
-router.delete('/:id', donutsCtrl.deleteDonut);
-router.put('/:id', donutsCtrl.updateDonut);
-router.get('/:id', donutsCtrl.getDonutById);
+router.delete('/:id', authenticateToken, donutsCtrl.deleteDonut);
+router.put('/:id', authenticateToken, donutsCtrl.updateDonut);
+router.get('/:id', authenticateToken, donutsCtrl.getDonutById);
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
 const usersCtrl = require('../controllers/usersCtrl');
+const authenticateToken = require('../middleware/authenticateToken');
 
 router.post('/login', usersCtrl.login);
-router.post('/changePassword', usersCtrl.changePassword);
+router.post('/changePassword', authenticateToken, usersCtrl.changePassword);
 
 module.exports = router;
