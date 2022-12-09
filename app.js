c45cdf953a958e76f5a1e07d5dcc1457c541cd32
JurRodi <rodijurrien@gmail.com>
2022-12-09 10:55:31 +0100

    Feat: add dotenv & cors and add conn top cloudDB

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
+require('dotenv').config()
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const rateLimit = require('express-rate-limit')
+var cors = require('cors')
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/donuttello', {useNewUrlParser: true});
+mongoose.connect(process.env.MONGO_URI);
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -23,6 +25,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors())
 
 const limiter = rateLimit({
 	windowMs: 60 * 60 * 1000,
